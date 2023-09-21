6033592b433f2c206d8e388f2614c14d974ab78a
nesta.web <nesta.web.dev@gmail.com>
2023-09-21 20:02:54 +0200

    revolver effect

--- app.js
+++ app.js
@@ -17,9 +17,9 @@ window.addEventListener("scroll", function() {
     if (animState == 3) {
         fourthTransition();
     }
-    // if (state == 4) {
-    //     fifthTransition();
-    // }
+    if (animState == 4) {
+        fifthTransition();
+    }
 });
 
 function firstTransition() {
@@ -167,10 +167,64 @@ function fourthTransition() {
             document.querySelector(".headerAnimTitle").innerText = "Italy";
         }
     }
+    if (window.scrollY > window.innerHeight * 6.3) {
+        animState = 4;
+    }
 }
 
-// Reduce weight iframe at loading
 
+function fifthTransition() {
+    if (window.scrollY < window.innerHeight * 6.3) {
+        document.querySelector(".circle").style.transform = "rotate(0) scale(0.85)";
+        document.querySelector(".valueText").style.transform = "rotate(0) scale(1.15)";
+        animState = 3;
+    }
+    else if (window.scrollY < window.innerHeight * 9) {
+        document.querySelector(".headerAnim3").style.transform = `translateY(${window.scrollY - (window.innerHeight * 6.3)}px)`;
+        if (window.scrollY < window.innerHeight * 7) {
+            document.querySelector(".circle").style.transform = "rotate(-45deg) scale(0.85)";
+            document.querySelector(".valueText").style.transform = "rotate(45deg) scale(1.15)";
+            document.querySelector(".valueTitle").innerText = "A GLOBAL NETWORK OF EXCELLENCE";
+            document.querySelector(".valueDesc").innerText = "To seize opportunities wherever they may be, 42 is opening up globally through its network of partner campuses in France and around the world. All the campuses are connected to mutualize their programs, knowledge and paths. After the core curriculum, you may continue your studies on one of the Network’s campuses to benefit from International experience. The diversity of profiles, nationalities and cultures allows you to broaden your perspectives.";
+        }
+        else if (window.scrollY < window.innerHeight * 7.3) {
+            document.querySelector(".circle").style.transform = "rotate(-90deg) scale(0.85)";
+            document.querySelector(".valueText").style.transform = "rotate(90deg) scale(1.15)";
+            document.querySelector(".valueTitle").innerText = "INCLUSIVE";
+            document.querySelector(".valueDesc").innerText = "Everyone can code and work in IT. We refuse gender stereotypes and welcome atypical profiles and people looking to branch out from a former path, in retraining or from disadvantaged backgrounds. We fight discrimination on a daily basis. Diversity is a strength that allows us to face ever-growing challenges together. Once you have reached majority, there are no age restrictions to apply to 42.";
+        }
+        else if (window.scrollY < window.innerHeight * 7.6) {
+            document.querySelector(".circle").style.transform = "rotate(-135deg) scale(0.85)";
+            document.querySelector(".valueText").style.transform = "rotate(135deg) scale(1.15)";
+            document.querySelector(".valueTitle").innerText = "THREE YEARS MORE OR LESS";
+            document.querySelector(".valueDesc").innerText = "We believe that laying the foundations of programming should happen in the pace and rhythm that matches with the students. Therefore, 42’s projects are not limited in time. Next to that, the only consequence of failing a project, is that you can try it again! Some students might move faster than others through the curriculum for this reason. Though, on average, our students reach level 21 in 3 years.";
+        }
+        else if (window.scrollY < window.innerHeight * 7.9) {
+            document.querySelector(".circle").style.transform = "rotate(-180deg) scale(0.85)";
+            document.querySelector(".valueText").style.transform = "rotate(180deg) scale(1.15)";
+            document.querySelector(".valueTitle").innerText = "NO COURSES. NO TEACHERS. NO CLASSES.";
+            document.querySelector(".valueDesc").innerText = "Thanks to its innovative teaching methods, 42 proposes high-quality training without having to resort to lectures. Different from traditional methods, students learn programming through a peer-to-peer learning method. That means that students learn programming from and with each other. The pedagogic staff is ever present to support and motivate the students, rather than to teach. In order to progress at 42, teamwork is required. You have to rely on the community to learn things that you then share with others. You don’t learn programming by copying algorithms on paper!";
+        }
+        else if (window.scrollY < window.innerHeight * 8.2) {
+            document.querySelector(".circle").style.transform = "rotate(-215deg) scale(0.85)";
+            document.querySelector(".valueText").style.transform = "rotate(215deg) scale(1.15)";
+            document.querySelector(".valueTitle").innerText = "NOT JUST A JOB: A CAREER";
+            document.querySelector(".valueDesc").innerText = "The training at 42 will not only grant you a job, it will train you for the jobs of tomorrow. At 42, you learn to learn so you keep growing professionally. Rather than offering a series of short training courses about specific technologies and languages that will quickly become obsolete, at 42 you learn  how to learn throughout your life in order to adapt to new technologies yet to be developed. With this skill in your pocket, you remain up-to-date for the jobs of tomorrow.";
+        }
+        else if (window.scrollY < window.innerHeight * 8.5) {
+            document.querySelector(".circle").style.transform = "rotate(-270deg) scale(0.85)";
+            document.querySelector(".valueText").style.transform = "rotate(270deg) scale(1.15)";
+            document.querySelector(".valueTitle").innerText = "NO DEGREE REQUIRED";
+            document.querySelector(".valueDesc").innerText = "Selection for entry at 42 is a positioning test that is not based on an academic record, standard test scores or a degree. We invite candidates who pass the first registration steps to spend a month with us in order to discover the way we work. This period offers them an opportunity to determine for themselves if 42’s methods suit them. Even though we do welcome those who are lo longer in school, we don’t encourage our applicants to stop their current studies. All applicants under 18 are required to have a high school degree or an equivalent.";
+        }
+        else if (window.scrollY < window.innerHeight * 8.8) {
+            document.querySelector(".circle").style.transform = "rotate(-315deg) scale(0.85)";
+            document.querySelector(".valueText").style.transform = "rotate(315deg) scale(1.15)";
+            document.querySelector(".valueTitle").innerText = "NO PRIOR EXPERIENCE IN CODING REQUIRED";
+            document.querySelector(".valueDesc").innerText = "The 42 training is complete. It starts from scratch for every student. You can apply to 42 even if you have never coded in your life. Though ambitious, the training course allows anyone who takes it to find a job and a career in IT.";
+        }
+    }
+}
 
 
 // function labnolIframe(div) {
